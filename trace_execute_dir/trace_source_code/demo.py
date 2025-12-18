b5ae9b83259313ace4529a872d06088e137cfa22
雷尚霖 <leishanglin@MBP-F2GX4KX7JV-2102.local>
2025-12-18 22:45:42 +0800

    update py-ide.html, trace-view.html, custom_style.html

--- trace_execute_dir/trace_source_code/demo.py
+++ trace_execute_dir/trace_source_code/demo.py
@@ -2,9 +2,9 @@
 import torch
 
 def main():
-    x = torch.tensor([1.0, 2.0, 3.0])
-    y = x * 2
-    z = y.sum()
+    x = torch.tensor([1.0, 2.0, 3.0]) # @inspect x
+    y = x * 2 # @inspect y
+    z = y.sum() # @inspect z
     print("Result:", z)
 
 main()
